4d463954941c9041f28e97c29f2493f8ca36df41
Mateus Dinanga <mateusclema@gmail.com>
2025-09-18 02:22:04 -0300

    Colocando o delay nas nossa páginas

--- app.js
+++ app.js
@@ -1,60 +1,63 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-import './src/database';
+import "./src/database";
 
-import { resolve } from 'path';
+import { resolve } from "path";
 
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import delay from "express-delay";
 
-
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import tokenRoutes from './src/routes/tokenRoutes';
-import alunoRoutes from './src/routes/alunoRoutes';
-import fotoRoutes from './src/routes/fotoRoutes';
+import homeRoutes from "./src/routes/homeRoutes";
+import userRoutes from "./src/routes/userRoutes";
+import tokenRoutes from "./src/routes/tokenRoutes";
+import alunoRoutes from "./src/routes/alunoRoutes";
+import fotoRoutes from "./src/routes/fotoRoutes";
 
 const whiteList = [
-  'http://localhost:3001',
-  'http://localhost:3000' //Depois veremos como colocar o domínio da aplicação
+  "http://localhost:3001",
+  "http://localhost:3000", //Depois veremos como colocar o domínio da aplicação
 ];
 
 const corsOption = {
-  origin: function(origin, callback){
-    if(whiteList.indexOf(origin) !== -1 || !origin){
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Não permitido pelo CORS'));
+      callback(new Error("Não permitido pelo CORS"));
     }
-  }
-}
+  },
+};
 
 class App {
-  constructor(){
-    this.app = express();// Cria uma instância do express;
-    this.middlewares();// Configura os middlewares que serão usados na aplicação;
+  constructor() {
+    this.app = express(); // Cria uma instância do express;
+    this.middlewares(); // Configura os middlewares que serão usados na aplicação;
     this.routes(); // Configura as rotas que serão usadas na aplicação;
   }
 
-  middlewares(){
-    this.app.use(cors(corsOption));// Habilita o CORS para permitir requisições de diferentes origens;
-    this.app.use(helmet());// Protege a aplicação de algumas vulnerabilidades conhecidas;
-    this.app.use(express.urlencoded({ extended: true }));// Permite receber dados do formulário no body da requisição;
+  middlewares() {
+    this.app.use(cors(corsOption)); // Habilita o CORS para permitir requisições de diferentes origens;
+    this.app.use(helmet()); // Protege a aplicação de algumas vulnerabilidades conhecidas;
+    this.app.use(delay(2500));
+    this.app.use(express.urlencoded({ extended: true })); // Permite receber dados do formulário no body da requisição;
     this.app.use(express.json());
-    this.app.use('/images/', express.static(resolve(__dirname, '.', 'uploads')));
+    this.app.use(
+      "/images/",
+      express.static(resolve(__dirname, ".", "uploads"))
+    );
   }
 
-  routes(){
-    this.app.use('/', homeRoutes);
-    this.app.use('/users/', userRoutes);
-    this.app.use('/tokens/', tokenRoutes);
-    this.app.use('/alunos/', alunoRoutes);
-    this.app.use('/fotos/', fotoRoutes);
+  routes() {
+    this.app.use("/", homeRoutes);
+    this.app.use("/users/", userRoutes);
+    this.app.use("/tokens/", tokenRoutes);
+    this.app.use("/alunos/", alunoRoutes);
+    this.app.use("/fotos/", fotoRoutes);
   }
 }
-
 
 export default new App().app;
